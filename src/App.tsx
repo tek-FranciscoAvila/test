a1f3d90342ac8331ee34b851640764deea10a62f
GustavOsorio <gustavo.osorio@tekchoice.com>
2024-10-25 13:59:34 -0600

    remove srore

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,14 @@
-import { Provider } from 'react-redux';
+// import { Provider } from 'react-redux';
 import { MantineProvider } from '@mantine/core';
-import { store } from '@/store';
 import { theme } from '@/configs';
 
 const App: React.FC = () => {
   return (
-    <Provider store={store}>
-      <MantineProvider theme={theme}>
-        <h1>
-          Hello world!
-        </h1>
-      </MantineProvider>
-    </Provider>
+    <MantineProvider theme={theme}>
+      <h1>
+        Hello world!
+      </h1>
+    </MantineProvider>
   );
 };
 
