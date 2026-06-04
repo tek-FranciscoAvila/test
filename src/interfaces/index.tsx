ea4f6a8c21f4a614d8c2ae0ecf49527e77d5d40b
tek-FranciscoAvila <francisco.avila@tekchoice.com>
2026-06-04 15:26:51 -0600

    Fix ESLint and TypeScript errors

--- src/interfaces/index.tsx
+++ src/interfaces/index.tsx
@@ -1,7 +1,7 @@
 //COMMON
 import { ButtonProps } from '@mantine/core';
 
-// @ts-ignore
+// @ts-expect-error
 export interface CustomButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement>, ButtonProps {
   text?: string;
   children?: React.ReactNode | string;
--- src/store/slices/counter-example/index.tsx
+++ src/store/slices/counter-example/index.tsx
@@ -14,14 +14,13 @@ export const counterSlice = createSlice({
   initialState,
   reducers: {
     increment: (state) => {
-
-      state.value += 1
+      state.value = Number(state.value) + 1
     },
     decrement: (state) => {
       state.value -= 1
     },
     incrementByAmount: (state, action: PayloadAction<number>) => {
-      state.value += action.payload
+      state.value = Number(state.value) + action.payload
     },
   },
 })
